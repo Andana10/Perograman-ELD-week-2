3b7a2057f613069850f918e8d81855370f0887c1
Andana10 <andanamahardeka@gmail.com>
2025-09-17 06:09:29 +0700

    Add files via upload

---
+++ aktivitas 1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int angka = 10; // Deklarasi beberapa angka
+	int desimal = 5.5;
+	int karakter = 'A';
+	// Deklarasi beberapa variabel integer dalam satu baris
+	int a = 1, b = 2, c=3;
+	cout << "Nilai angka: " << angka << ::endl;
+	cout << "Nilai desimal: " << desimal << ::endl;
+	cout << "Nilai karakter: " << karakter << ::endl;
+	cout << "Nilai a: " << a << ::endl;
+	cout << "Nilai b: " << b << ::endl;
+	cout << "Nilai c: " << c << ::endl;
+	return 0;
+}
---
+++ aktivitas 2.2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+	string kalimat;
+	cin.ignore();
+	cout << "Masukkan sebuah kalimat: ";
+	getline(cin, kalimat);
+	cout << "Kalimat yang Anda masukkan: " << kalimat << endl;
+	return 0;
+}
